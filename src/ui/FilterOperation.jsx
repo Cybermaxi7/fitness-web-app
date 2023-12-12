11a67a34bc67fdcff0b9ec066f8b03f0fdc07eab
cybermaxi7 <maximusagbe@gmail.com>
2023-12-12 03:07:45 +0100

    removed the lazy loading from the image

--- src/ui/FilterOperation.jsx
+++ src/ui/FilterOperation.jsx
@@ -7,7 +7,7 @@ export default function FilterOperation({ scrollToRef }) {
     const { data: parts, isLoading: isLoadingBodyPart } = useBodyPartList();
     const { data: targets, isLoading: isLoadingTargets } = useTargetList();
     return (
-        <div className="flex flex-col gap-8">
+        <div className="flex flex-col gap-8 mb-8">
             <div className="flex gap-2 items-center justify-center flex-col flex-wrap">
                 <h2>Filter By BodyPart</h2>
                 {parts ? (
